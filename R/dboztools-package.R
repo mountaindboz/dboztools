d00032e87bc09ce59f5832806623891505b20e9f
Dave Bosworth <David.Bosworth@water.ca.gov>
2021-02-20 14:15:23 -0800

    Adding package documentation

---
+++ R/dboztools-package.R
@@ -0,0 +1,14 @@
+#' My Utilities Package
+#'
+#' A collection of commonly-used functions and RMarkdown templates.
+#'
+#' @keywords internal
+#' @docType package
+#' @name dboztools
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
